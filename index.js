9447c873f530cd333e8593eb435da5985f6761b6
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-08-29 13:29:55 +0100

    changed the DOM elements name

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("card-el")
+let cardEl = document.getElementById("card-el")
 
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
+    cardEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+        cardEl.textContent += cards[i] + " "
     }
     
     sumEl.textContent = "Sum: " + sumCard
